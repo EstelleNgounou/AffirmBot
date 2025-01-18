5842c70bc27508b22a48ee296c1716b333da7610
engou005 <engou005@uottawa.ca>
2025-01-18 18:40:04 -0500

    Update AffirmBot.py

--- AffirmBot.py
+++ AffirmBot.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import os
 
 from chat import get_response
 
@@ -17,3 +18,5 @@ def predict():
 
 if __name__== "__main__":
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5500))  
+    app.run(host="0.0.0.0", port=port)
